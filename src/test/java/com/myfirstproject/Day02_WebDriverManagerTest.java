3c4f71b0907909873f792ec947a04881813ff702
Emre Ozer <constellation2023@gmail.com>
2023-07-15 21:20:54 +0300

    15/7/2023

--- src/test/java/com/myfirstproject/Day02_WebDriverManagerTest.java
+++ src/test/java/com/myfirstproject/Day02_WebDriverManagerTest.java
@@ -2,8 +2,6 @@ package com.myfirstproject;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Day02_WebDriverManagerTest {
---
+++ src/test/java/com/myfirstproject/practices/practice01/Q03_BeforeAnnotation.java
@@ -0,0 +1,56 @@
+package com.myfirstproject.practices.practice01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Q03_BeforeAnnotation {
+    // Create chrome driver by using @Before annotation and WebDriverManager
+// Navigate to https://testpages.herokuapp.com/styled/index.html
+// Click on Calculator under Micro Apps
+// Type any number in the first input
+// Type any number in the second input
+// Click on Calculate
+// Get the result
+// Verify the result
+// Print the result
+// Close the browser by using @After annotation
+
+WebDriver driver;
+@Before
+public void setUp(){
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Dynamic wait
+}
+
+@Test
+    public void test01(){
+    driver.get("https://testpages.herokuapp.com/styled/index.html");
+    driver.findElement(By.id("calculatetest")).click();
+    String input1 = "6";
+    driver.findElement(By.id("number1")).sendKeys(input1);
+    String input2 = "4";
+    driver.findElement(By.id("number2")).sendKeys(input2);
+    driver.findElement(By.id("calculate")).click();
+    String result = driver.findElement(By.id("answer")).getText();
+    assertEquals(Integer.parseInt(input1)+Integer.parseInt(input2)+"", result); //verifying the result. Could hae used "valueOf" instead of "parseInt" also
+    System.out.println("result = " + result);
+}
+
+@After
+    public void tearDown() throws InterruptedException {
+    Thread.sleep(3000);
+    driver.quit();
+}
+}
