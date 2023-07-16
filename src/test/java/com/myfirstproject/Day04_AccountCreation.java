25ad278e6ab24df64a856c1cf16fe4a1a789eb57
Emre Ozer <constellation2023@gmail.com>
2023-07-16 19:46:11 +0300

    16/7/2023

--- src/test/java/com/myfirstproject/Day04_AccountCreation.java
+++ src/test/java/com/myfirstproject/Day04_AccountCreation.java
@@ -1,5 +1,46 @@
 package com.myfirstproject;
 
-public class Day04_AccountCreation {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class Day04_AccountCreation {
+    /*  1. Launch browser - DONE IN setUp
+        2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        3. Verify that home page is visible successfully
+        Assert.assertTrue(driver.getTitle().equals("Automation Exercise"));
+        4. Click on 'Signup / Login' button
+        driver.findElement(By.partialLinkText("Signup / Login")).click();
+        WebElement signUpLink = driver.findElement(By.partialLinkText("Signup / Login"));
+        signUpLink.click();
+        automationexercise.com
+        Automation Exercise
+        This is for automation practice
+    */
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void accountCreationTest(){
+        driver.get("https://www.automationexercise.com");
+        Assert.assertTrue(driver.getTitle().equals("Automation Exercise"));
+        driver.findElement(By.partialLinkText("Signup / Login")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//h2[.='New User Signup!']")).isDisplayed());
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Johnny Walker");
+
+    }
 }
