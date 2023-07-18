ee9f5f0df959659f5481a0c49f42e1bdfe0bd33e
Emre Ozer <constellation2023@gmail.com>
2023-07-18 17:05:39 +0300

    18/7/2023

---
+++ src/test/java/com/myfirstproject/Day04_TestBaseTest.java
@@ -0,0 +1,16 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Test;
+
+public class Day04_TestBaseTest extends TestBase {
+
+
+    @Test
+    public void testBaseTest(){
+        driver.get("https://www.amazon.com");
+
+
+    }
+
+}
