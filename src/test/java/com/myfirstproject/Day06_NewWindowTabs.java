8cebc1602dfcad29ef8619e99c03562165a8812c
Emre Ozer <constellation2023@gmail.com>
2023-07-22 19:09:00 +0300

    22/7/2023

---
+++ src/test/java/com/myfirstproject/Day06_NewWindowTabs.java
@@ -0,0 +1,59 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WindowType;
+
+public class Day06_NewWindowTabs extends TestBase {
+
+    @Test
+    public void newTabTest() throws InterruptedException {
+
+        driver.get("https://www.linkedin.com");
+        //Assert.assertTrue(driver.getTitle().contains("Linkedin"));
+        String window1Handle = driver.getWindowHandle();
+        System.out.println("Window 1 = " + window1Handle);
+        Thread.sleep(5000);
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.ebay.com");
+        String eBayWindowHandle = driver.getWindowHandle();
+        System.out.println("Window 2 = " + eBayWindowHandle);
+        Assert.assertTrue(driver.getTitle().contains("eBay"));
+        Thread.sleep(5000);
+        //At this point the driver is on the eBay tab
+        driver.switchTo().window(window1Handle);
+        System.out.println("Linkedin Title " + driver.getTitle());
+        Thread.sleep(5000);
+        driver.switchTo().window(eBayWindowHandle);
+        System.out.println("eBay Title " +driver.getTitle());
+        Thread.sleep(5000);
+
+    }
+
+    @Test
+    public void newWindowTest() throws InterruptedException {
+
+        driver.get("https://www.linkedin.com");
+        //Assert.assertTrue(driver.getTitle().contains("Linkedin"));
+        String window1Handle = driver.getWindowHandle();
+        System.out.println("Window 1 = " + window1Handle);
+        Thread.sleep(5000);
+
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.ebay.com");
+        String eBayWindowHandle = driver.getWindowHandle();
+        System.out.println("Window 2 = " + eBayWindowHandle);
+        Assert.assertTrue(driver.getTitle().contains("eBay"));
+        Thread.sleep(5000);
+        //At this point the driver is on the eBay tab
+        driver.switchTo().window(window1Handle);
+        System.out.println("Linkedin Title " + driver.getTitle());
+        Thread.sleep(5000);
+        driver.switchTo().window(eBayWindowHandle);
+        System.out.println("eBay Title " +driver.getTitle());
+        Thread.sleep(5000);
+
+    }
+}
