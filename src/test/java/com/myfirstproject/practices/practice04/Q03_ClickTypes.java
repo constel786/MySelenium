f3766f3161728be4054441bd2491319a188948d5
Emre Ozer <constellation2023@gmail.com>
2023-07-27 19:00:47 +0300

    27/7/2023

--- src/test/java/com/myfirstproject/practices/practice04/Q03_ClickTypes.java
+++ src/test/java/com/myfirstproject/practices/practice04/Q03_ClickTypes.java
@@ -3,7 +3,9 @@ package com.myfirstproject.practices.practice04;
 import com.myfirstproject.utilities.TestBase;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class Q03_ClickTypes extends TestBase {
 
@@ -14,14 +16,36 @@ public class Q03_ClickTypes extends TestBase {
         driver.get("https://testpages.herokuapp.com/styled/events/javascript-events.html");
         WebElement onblur = driver.findElement(By.id("onblur"));
         WebElement onclick = driver.findElement(By.id("onclick"));
-        onblur.click();
-        Thread.sleep(2000);
-        onclick.click(); //This click will not trigger onblur
-        Thread.sleep(2000);
-        onblur.click(); //We need to click on onclick one more time to trigger it
-        Thread.sleep(2000);
+        WebElement oncontextmenu = driver.findElement(By.id("oncontextmenu"));
+        WebElement ondoubleclick = driver.findElement(By.id("ondoubleclick"));
+        WebElement onfocus = driver.findElement(By.id("onfocus"));
+        WebElement onkeydown = driver.findElement(By.id("onkeydown"));
+        WebElement onkeyup = driver.findElement(By.id("onkeyup"));
+        WebElement onkeypress = driver.findElement(By.id("onkeypress"));
+        WebElement onmouseover = driver.findElement(By.id("onmouseover"));
+        WebElement onmouseleave = driver.findElement(By.id("onmouseleave"));
+        WebElement onmousedown = driver.findElement(By.id("onmousedown"));
+
+        //To do a right-click we need to use the action object
+        Actions actions = new Actions(driver);
+        actions                                 //method chain
+                .click(onblur)
+                .click(onclick) //This click will not trigger onblur
+                .click(onclick) //We need to click on onclick one more time to trigger it
+                .contextClick(oncontextmenu)
+                .doubleClick(ondoubleclick)
+                .click(onfocus)
+                .click(onkeydown).sendKeys(Keys.ENTER) //To trigger onkeydown button, we need to press any key, such as enter
+                .click(onkeyup).sendKeys(Keys.SPACE) //Will trigger when for example spacebar is released
+                .click(onkeypress).sendKeys(Keys.SPACE)
+                .click(onmouseover)
+                .moveToElement(onmouseover)
+                .moveToElement(onmouseleave) //Need to move mouse to anohter place to trigger
+                .moveToElement(onmouseover) //Moved mouse to another place, onmouseover button
+                .click(onmousedown)
+                .perform(); //Add last. Does not work without perform()
+
+        Thread.sleep(1000);
 
     }
-
-
 }
