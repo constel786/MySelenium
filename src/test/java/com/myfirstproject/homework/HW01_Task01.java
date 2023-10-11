6952ffdb0aa3bfae24b9d82c44cf10b9e8d73f48
constel786 <134733862+constel786@users.noreply.github.com>
2023-10-11 19:21:40 +0300

    11/10/2023

--- src/test/java/com/myfirstproject/homework/HW01_Task01.java
+++ src/test/java/com/myfirstproject/homework/HW01_Task01.java
@@ -17,7 +17,7 @@ public class HW01_Task01 extends TestBase {
 //    Click Sign Up
     @Test
     public void task01(){
-//        driver.get("https://www.facebook.com/ ");
+        driver.get("https://www.facebook.com/ ");
 //        driver.findElement(By.xpath("//div//a[@id='u_0_0_5p']")).click();
 //        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("James");
 //        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Brown");
